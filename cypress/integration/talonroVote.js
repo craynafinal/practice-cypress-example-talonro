0a98d7c2bbf67e2469a5fd9147fc66ac7fff5981
craynafinal <craynafinal@gmail.com>
2019-02-03 15:16:06 -0800

    fixed package.json

--- cypress/integration/talonroVote.js
+++ cypress/integration/talonroVote.js
@@ -1,15 +1,22 @@
-describe('TalonRO Vote', function() {
-	it('Votes for captcha free ranking sites', function() {
-		cy.clearCookies()
-		cy.clearLocalStorage()
-		cy.visit('https://panel.talonro.com')
-		cy.get('#auth').type('')
-		cy.get('#password').type('')
-		cy.get('#remember_me_checkbox').click()
-		cy.get('#elSignIn_submit').click()
-		cy.visit('https://panel.talonro.com/voting/')
-		cy.get(':nth-child(4) > :nth-child(1) > form > .button').click({force:true})
-		cy.visit('http://www.xtremetop100.com/in.php?site=1132205111')
-		cy.get('[type="submit"]').click()
-	})
+/*describe('TalonRO Vote', function() {
+  it('Votes for captcha free ranking sites', function() {
+    cy.clearCookies()
+    cy.clearLocalStorage()
+    cy.visit('https://panel.talonro.com')
+    cy.get('#auth').type('')
+    cy.get('#password').type('')
+    cy.get('#remember_me_checkbox').click()
+    cy.get('#elSignIn_submit').click()
+    cy.visit('https://panel.talonro.com/voting/')
+    cy.get(':nth-child(4) > :nth-child(1) > form > .button').click({force:true})
+    cy.visit('http://www.xtremetop100.com/in.php?site=1132205111')
+    cy.get('[type="submit"]').click()
+  })
+})*/
+
+describe('Test', function() {
+  it('Test', function() {
+    cy.visit('https://google.com')
+  })
 })
+
